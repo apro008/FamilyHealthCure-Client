d3faa7aca07757c184eca2d7449e408a1beb9469
Aprajati Siuli <aprajati.siuli@rhatechnologies.com>
2025-08-03 04:34:45 +0530

    add email

--- src/utils/contactInfo.js
+++ src/utils/contactInfo.js
@@ -23,7 +23,8 @@ export const contactInfo = {
 	},
 };
 
-export const email = "familyhealthcare60@gmail.com";
+export const email = `${"familyhealthcare60@gmail.com"}
+${"familyhealthcarekolkata@gmail.com"}`;
 
 export const address = "455, Boral Main Road, kolkata-700154";
 
